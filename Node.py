194d5e4541526dac9bd368d5d05a86ebc1d9fb34
Mohammed-Shafeey <hammodyshafie@hotmail.com>
2025-04-16 20:51:32 +0300

    removed node

--- Node.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self, x: float, y: float, z: float, orientation: float, next: Node, prev: Node):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.next = next
-        self.prev = prev
-        self.orientation = orientation  #in degrees 
